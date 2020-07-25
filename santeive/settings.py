149d11b7e4584ad9c011bed85b7ab027f2918f75
santeive <packo.ive@gmail.com>
2020-07-25 15:32:24 -0500

    sqlite deleted

--- santeive/settings.py
+++ santeive/settings.py
@@ -62,13 +62,6 @@ WSGI_APPLICATION = 'santeive.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-""" 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-} """
 
 import dj_database_url
 DATABASES = {
