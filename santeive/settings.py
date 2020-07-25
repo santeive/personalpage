8983dcb7634f97f3d4f50008f243b878498d56e9
santeive <packo.ive@gmail.com>
2020-07-25 16:31:24 -0500

    detached from

--- santeive/settings.py
+++ santeive/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# Static files
+# My Static Files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
