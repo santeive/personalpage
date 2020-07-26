395934c30dbe949d55db0915a4ddeabf0cb2c421
santeive <packo.ive@gmail.com>
2020-07-26 17:06:39 -0500

    My static false

--- santeive/settings.py
+++ santeive/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 """ DEBUG = config('DEBUG', cast=bool) """
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.santeive.me', '127.0.0.1', 'santeiveblg.herokuapp.com']
 
