f5ac5afeac48a70c225cf6a4e149e416cc4dbbfb
santeive <packo.ive@gmail.com>
2020-07-26 14:37:38 -0500

    static files

--- santeive/settings.py
+++ santeive/settings.py
@@ -109,11 +109,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # My Static Files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+   ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = '.storage.WhiteNoiseStaticFilesStorage'
