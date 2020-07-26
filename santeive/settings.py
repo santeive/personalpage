290c9fe7a8d6ef8f62580cc66a028fa3ae28ed2d
santeive <packo.ive@gmail.com>
2020-07-26 16:59:52 -0500

    True wwwa

--- santeive/settings.py
+++ santeive/settings.py
@@ -112,6 +112,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
